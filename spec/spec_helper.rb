1deb8828fc2aee03d28fbdb5fcd69f7cc9b9fbc6
Jesse R. Adams <jesse.adams@stelligent.com>
2018-10-26 09:20:57 -0700

    Purge potential env vars

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,8 @@ SimpleCov.start do
 end
 
 ENV['NO_PROXY'] = '127.0.0.1,localhost,circleci-internal-outer-build-agent'
+ENV.delete('http_proxy')
+ENV.delete('https_proxy')
+ENV.delete('no_proxy')
 
 RSpec::Expectations.configuration.on_potential_false_positives = :nothing
