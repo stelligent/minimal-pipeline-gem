765136611f033fc9c197046eaa0ce794d26d4ebd
Jesse R. Adams <jesse.adams@stelligent.com>
2018-12-11 10:47:52 -0800

    Cleaning up more requires

--- lib/minimal_pipeline.rb
+++ lib/minimal_pipeline.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
-require 'aws-sdk'
-require 'crossing'
-require 'keystore'
-require 'open3'
-require 'docker'
-require 'json'
-
 # Top level namespace for automation pipeline classes
 class MinimalPipeline
   # autoload libraries
